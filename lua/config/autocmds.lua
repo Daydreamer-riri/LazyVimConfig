25f8a482f2683d9d9389fb98e5865e5fac1fc291
Riri <Daydreamerriri@outlook.com>
2024-08-13 00:43:00 +0800

    update eslint

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,13 +2,23 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-vim.cmd([[
-  autocmd BufWritePost *.tsx,*.ts,*.jsx,*.js,*.json,*.scss EslintFixAll
-]])
+-- vim.cmd([[
+--   autocmd BufWritePost *.tsx,*.ts,*.jsx,*.js,*.json,*.scss EslintFixAll
+-- ]])
 
 -- -- Disable autoformat for lua files
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "typescriptreact", "javascriptreact", "typescript", "javascript", "json", "jsonc", "sass", "scss" },
+  pattern = {
+    "typescriptreact",
+    "javascriptreact",
+    "typescript",
+    "javascript",
+    "json",
+    "jsonc",
+    "sass",
+    "scss",
+    "markdown",
+  },
   callback = function()
     vim.b.autoformat = false
   end,
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -16,56 +16,59 @@ return {
           -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
           workingDirectories = { mode = "auto" },
 
-          eslint = {
-            filetypes = {
-              "javascript",
-              "typescript",
-              "javascriptreact",
-              "typescriptreact",
-              "json",
-              "jsonc",
-              "json5",
-              "scss",
-              "css",
-            },
-          },
+          -- eslint = {
+          --   filetypes = {
+          --     "javascript",
+          --     "typescript",
+          --     "javascriptreact",
+          --     "typescriptreact",
+          --     "json",
+          --     "jsonc",
+          --     "json5",
+          --     "scss",
+          --     "css",
+          --   },
+          -- },
         },
         filetypes = {
           "javascript",
-          "typescript",
           "javascriptreact",
+          "javascript.jsx",
+          "typescript",
           "typescriptreact",
+          "typescript.tsx",
+          "vue",
+          "html",
+          "markdown",
           "json",
           "jsonc",
-          "json5",
-          "scss",
+          "yaml",
+          "toml",
+          "xml",
+          "gql",
+          "graphql",
+          "astro",
+          "svelte",
           "css",
+          "less",
+          "scss",
         },
       },
     },
     setup = {
-      eslint = function()
+      eslint = function(_, opts)
         require("lazyvim.util").lsp.on_attach(function(client, bufnr)
           client.server_capabilities.documentRangeFormattingProvider = true
           if client.name == "eslint" then
             client.server_capabilities.documentFormattingProvider = true
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              buffer = bufnr,
+              command = "EslintFixAll",
+            })
           elseif client.name == "tsserver" then
             client.server_capabilities.documentFormattingProvider = false
           end
         end)
-        -- local function get_client(buf)
-        --   return LazyVim.lsp.get_clients({ name = "eslint", bufnr = buf })[1]
-        -- end
-        --
-        -- local formatter = LazyVim.lsp.formatter({
-        --   name = "eslint: lsp",
-        --   primary = false,
-        --   priority = 200,
-        --   filter = "eslint",
-        -- })
-        --
-        -- -- register the formatter with LazyVim
-        -- LazyVim.format.register(formatter)
       end,
     },
   },
