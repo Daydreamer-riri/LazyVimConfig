055b43d6064f06588e269483c557b7519928b379
ding.zhao <ding.zhao@trip.com>
2024-09-02 14:19:58 +0800

    diffview

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -5,10 +5,10 @@ return {
     version = "*",
     opts = {
       default_mappings = {
-        ours = "<leader>gho",
-        theirs = "<leader>ght",
-        none = "<leader>gh0",
-        both = "<leader>ghb",
+        ours = "<leader>ho",
+        theirs = "<leader>ht",
+        none = "<leader>h0",
+        both = "<leader>hb",
         next = "]x",
         prev = "[x",
       },
@@ -23,6 +23,64 @@ return {
         "<leader>gr",
         "<cmd>GitConflictRefresh<cr>",
         desc = "Refresh Conflicts",
+      },
+    },
+  },
+  {
+    "sindrets/diffview.nvim",
+    opts = {
+      view = {
+        merge_tool = {
+          -- Config for conflicted files in diff views during a merge or rebase.
+          layout = "diff4_mixed",
+          disable_diagnostics = true, -- Temporarily disable diagnostics for diff buffers while in the view.
+          winbar_info = true, -- See |diffview-config-view.x.winbar_info|
+        },
+      },
+      default_args = { -- Default args prepended to the arg-list for the listed commands
+        DiffviewOpen = { "--imply-local" },
+      },
+    },
+    keys = {
+      {
+        "<leader>gv",
+        "<cmd>DiffviewOpen<cr>",
+        desc = "open diffview",
+      },
+    },
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    keys = {
+      {
+        "<leader>hb",
+        "<cmd>Gitsigns blame_line<cr>",
+        desc = "Blame Line",
+      },
+      {
+        "<leader>hs",
+        "<cmd>Gitsigns stage_hunk<cr>",
+        desc = "Stage Hunk",
+      },
+      {
+        "<leader>hS",
+        "<cmd>Gitsigns stage_buffer<cr>",
+        desc = "Stage Buffer",
+      },
+      {
+        "<leader>hr",
+        "<cmd>Gitsigns reset_hunk<cr>",
+        desc = "Reset Hunk",
+      },
+      {
+        "<leader>hR",
+        "<cmd>Gitsigns reset_buffer<cr>",
+        desc = "Reset Buffer",
+      },
+      {
+        "<leader>hu",
+        "<cmd>Gitsigns undo_stage_hunk<cr>",
+        desc = "Undo Stage Hunk",
       },
     },
   },
