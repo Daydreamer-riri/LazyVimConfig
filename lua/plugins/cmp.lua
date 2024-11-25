cd36e070419340093c1b1b21dad956c09082b552
Riri <Daydreamerriri@outlook.com>
2024-11-26 02:00:48 +0800

    feat: cmp style

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -62,9 +62,14 @@ return {
       }
 
       -- local window = require("cmp.config.window")
-      -- opts.window = opts.window or {}
+      opts.window = opts.window or {}
       -- opts.window.completion = window.bordered({ winhighlight = "Normal:Normal" })
-      -- opts.window.documentation = window.bordered()
+      -- opts.window.documentation = window.bordered({
+      --   winhighlight = "Normal:Pmenu",
+      -- })
+      opts.window.documentation = {
+        winhighlight = "Normal:Pmenu,FloatBorder:Pmenu",
+      }
     end,
   },
   {
