b88a9207525bc275b68cc87b04c235b8c7d6449e
Riri <Daydreamerriri@outlook.com>
2024-12-30 10:31:44 +0800

    feat: pretty telescope

--- lua/plugins/extend-telescope.lua
+++ lua/plugins/extend-telescope.lua
@@ -44,16 +44,19 @@ return {
         desc = "Grep with Args (root dir)",
       },
     },
-    config = function(_, opts)
+    opts = function(_, opts)
       vim.api.nvim_set_hl(0, "TelescopeBorder", { link = "FloatBorder" })
       vim.api.nvim_set_hl(0, "TelescopeNormal", { link = "NormalFloat" })
-      local telescope = require("telescope")
+      local origin_make_entry = require("telescope.make_entry")
+      local custom_make_entry = require("custom.telescope.make_entry")
+      for key, value in pairs(custom_make_entry) do
+        origin_make_entry[key] = value
+      end
 
       opts.defaults = vim.tbl_deep_extend("force", opts.defaults, {
-        wrap_results = true,
         layout_config = { prompt_position = "top" },
         sorting_strategy = "ascending",
-        path_display = { "filename_first" },
+        path_display = { "filename_first", "truncate" },
       })
       opts.extensions = {
         live_grep_args = {
@@ -70,9 +73,7 @@ return {
         },
       }
 
-      telescope.setup(opts)
-      require("telescope").load_extension("fzf")
-      require("telescope").load_extension("package_info")
+      return opts
     end,
   },
 }
