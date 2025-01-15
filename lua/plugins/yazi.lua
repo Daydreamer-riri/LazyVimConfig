3a7afaf2b73921e04954754c5caa870a71e15275
Riri <daydreamerriri@outlook.com>
2025-01-15 21:15:31 +0800

    feat(yazi): snacks picker and backdrop

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -1,3 +1,5 @@
+local backdrop = nil
+
 return {
   "Daydreamer-riri/yazi.nvim", -- support nushell on windows
   event = "VeryLazy",
@@ -21,9 +23,11 @@ return {
       desc = "Resume the last yazi session",
     },
   },
+  --- @module 'yazi'
+  ---@type fun(_: any, opts:YaziConfig):YaziConfig
   opts = function(_, opts)
     vim.api.nvim_set_hl(0, "YaziFloat", { link = "NormalFloat", default = true })
-    return vim.tbl_extend("force", opts, {
+    return vim.tbl_deep_extend("force", opts, {
       -- if you want to open yazi instead of netrw, see below for more info
       open_for_directories = false,
       open_multiple_tabs = true,
@@ -31,10 +35,37 @@ return {
       -- enable these if you are using the latest version of yazi
       use_ya_for_events_reading = true,
       use_yazi_client_id_flag = true,
-      highlight_hovered_buffers_in_same_directory = true,
+      highlight_hovered_buffers_in_same_directory = false,
 
       keymaps = {
         show_help = "<f1>",
+        replace_in_directory = "<c-r>",
+      },
+      integrations = {
+        grep_in_directory = function(dir)
+          Snacks.picker.grep({
+            dirs = { dir },
+            on_show = function()
+              vim.api.nvim_feedkeys("i", "n", false)
+            end,
+          })
+        end,
+      },
+      hooks = {
+        yazi_opened = function()
+          backdrop = Snacks.win({
+            style = "float",
+            focusable = false,
+            height = 0.1,
+            width = 0.1,
+          })
+        end,
+        yazi_closed_successfully = function()
+          if backdrop then
+            backdrop:close()
+          end
+          backdrop = nil
+        end,
       },
     })
   end,
