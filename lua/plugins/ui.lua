c92578863e3a55df9e2a55a6ed88a857fe8e8fd1
Riri <daydreamerriri@outlook.com>
2025-01-17 17:04:12 +0800

    feat: match no transparent

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -279,7 +279,7 @@ return {
     "sphamba/smear-cursor.nvim",
     opts = {
       cursor_color = "#d4be98",
-      legacy_computing_symbols_support = true,
+      -- legacy_computing_symbols_support = true,
       -- stiffness = 0.8, -- 0.6      [0, 1]
       trailing_stiffness = 0.3, -- 0.25     [0, 1]
       distance_stop_animating = 0.2, -- 0.1      > 0
