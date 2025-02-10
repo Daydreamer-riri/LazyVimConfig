8bbe7a3526b341f4aea231382a930b9917dd3181
Riri <daydreamerriri@outlook.com>
2025-02-10 19:45:57 +0800

    chore: update lualine style

--- lua/colorschemes/rose-pine.lua
+++ lua/colorschemes/rose-pine.lua
@@ -63,7 +63,8 @@ return {
     event = "VeryLazy",
     opts = function(_, opts)
       opts.options.section_separators = { right = "", left = "" }
-      opts.options.component_separators = { right = "|", left = "|" }
+      -- opts.options.component_separators = { right = "|", left = "|" }
+      opts.options.component_separators = { right = " ", left = " " }
     end,
   },
   {
